be766c8c4f96014cdf7b8c36d8078f3796da9c1d
ahza64 <ahza64@yahoo.com>
2017-08-05 20:19:50 -0700

    success calculator json req json res cycle

--- ultra_marine/main.py
+++ ultra_marine/main.py
@@ -14,9 +14,14 @@ def hello_world():
 
 @app.route('/calculator', methods=['POST'])
 def calculator():
-    print "CUSSSSS: ", request.get_json()
-    if request.get_json()["name"] == "postman":
-        return "these aren't the droids you're looking for"
+    n1 = int(request.get_json()['n1'])
+    n2 = int(request.get_json()['n2'])
+    op = request.get_json()['op']
+    ab = foo.Calc(n1, op, n2)
+    totalNum = {
+        "total": ab.total()
+    }
+    return jsonify(totalNum)
 
 if __name__ == "__main__":
 
