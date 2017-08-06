ed1a5b7fb277415d915a6a99d6d25fb240df6150
ahza64 <ahza64@yahoo.com>
2017-08-05 18:00:22 -0700

    import flask, setup server, abstract class

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(
+    name="ultra_marine",
+    packages=['ultra_marine']
+)
--- ultra_marine/main.py
+++ ultra_marine/main.py
@@ -2,8 +2,7 @@
 practice python calculator
 """
 import sys
-# from ultra_marine import calc
-# Calc = calc.Calc()
+import foo
 
 from flask import Flask
 app = Flask(__name__)
@@ -12,27 +11,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-class Calc:
-    num1 = None
-    num2 = None
-    oper = None
-
-    def __init__(self, num1, oper, num2):
-        self.num1 = int(num1)
-        self.num2 = int(num2)
-        self.oper = oper
-
-    def total(self):
-        if self.oper == "+":
-            return self.num1 + self.num2
-        if self.oper == "-":
-            return self.num1 - self.num2
-        if self.oper == "x":
-            return self.num1 * self.num2
-        if self.oper == "/":
-            return self.num1 / self.num2
+@app.route('/calculator')
+def calculator():
+    return "these aren't the droids you're looking for"
 
 if __name__ == "__main__":
 
-    c = Calc(sys.argv[1], sys.argv[2], sys.argv[3])
+    c = foo.Calc(sys.argv[1], sys.argv[2], sys.argv[3])
     print c.total()
